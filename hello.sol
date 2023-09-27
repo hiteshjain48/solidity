c388a67683697af6a13d3a0134723fc0c275a72f
hiteshjain48 <hiteshjainhd@gmail.com>
2023-09-27 23:53:08 +0530

    solidity started

---
+++ hello.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.17;
+
+contract Hello {
+    string public message;
+    function Hello (string initial) public {
+        message = initial;
+    }
+}
