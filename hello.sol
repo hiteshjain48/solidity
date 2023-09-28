e81763d6a81ebe8f879111cf116a86dc1c31df3c
hiteshjain48 <hiteshjainhd@gmail.com>
2023-09-28 23:11:03 +0530

    boilerplate

--- hello.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.4.17;
-
-contract Hello {
-    string public message;
-    function Hello (string initial) public {
-        message = initial;
-    }
-}
