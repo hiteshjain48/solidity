84741c359d4fbc157519190a53a408777760b8ec
hiteshjain48 <hiteshjainhd@gmail.com>
2023-09-28 23:35:37 +0530

    compile complete

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,9 @@
-// compile code will go here
+// compile code will go 
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
+
+inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol")
+const source = fs.readFileSync(inboxPath, 'utf-8')
+
+console.log(solc.compile(source, 1))
