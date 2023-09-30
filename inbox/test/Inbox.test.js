8d79adf87a295c2f598301d14302a83093eed360
hiteshjain48 <hiteshjainhd@gmail.com>
2023-09-30 18:14:24 +0530

    improved tests

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -13,9 +13,18 @@ class Car {
     }
 }
 
+let car
+
+beforeEach(()=>{
+    car = new Car()
+})
+
 describe('Car', ()=>{
     it('can park', ()=>{
-        car = new Car()
         assert.equal(car.park(), 'stopped')
+    })
+
+    it('can drive', ()=>{
+        assert.equal(car.drive(), 'vroom')
     })
 })
