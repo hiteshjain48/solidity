8231b2dba71fd796bedecd9246b40d1b80a9cd7f
hiteshjain48 <hiteshjainhd@gmail.com>
2023-10-01 21:08:36 +0530

    changes in tests

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
-
+const { interface, bytecode } = require('../compile')
 class Car {
     park(){
         return 'stopped'
@@ -13,18 +13,36 @@ class Car {
     }
 }
 
-let car
+// let car
 
-beforeEach(()=>{
-    car = new Car()
+// beforeEach(()=>{
+//     car = new Car()
+// })
+
+// describe('Car', ()=>{
+//     it('can park', ()=>{
+//         assert.equal(car.park(), 'stopped')
+//     })
+
+//     it('can drive', ()=>{
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// })
+
+let accounts
+let inbox 
+beforeEach(async () => {
+    //-----------------------------------
+    // get list of all accounts
+    // web3.eth.getAccounts().then(fetchedAccounts => {
+    //     console.log(fetchedAccounts)
+    // })
+    accounts = await web3.eth.getAccounts()
+    // Use one of the accounts to deploy the contract
+    inbox = web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: ['Hitesh here!!']}).send({from: accounts[0], gas:'1000000'})
+
 })
 
-describe('Car', ()=>{
-    it('can park', ()=>{
-        assert.equal(car.park(), 'stopped')
-    })
-
-    it('can drive', ()=>{
-        assert.equal(car.drive(), 'vroom')
-    })
+describe('Inbox', () => {
+    it('deploy a contract', () => {})
 })
