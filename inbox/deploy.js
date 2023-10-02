8d8bc8f2ed0e2ac3f48855cf8abbfc3193614f64
hiteshjain48 <hiteshjainhd@gmail.com>
2023-10-02 23:56:44 +0530

    deployment script using infura

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require('web3')
+const { interface, bytecode } = requrie('./compile')
+
+const provider = new HDWalletProvider(
+    'mnemonic',
+    'link'
+)
+
+const web3 = Web3(provider)
